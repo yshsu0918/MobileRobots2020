c21b862f172acdbf10af5ed8200651304af72bad
Yu-Shan Hsu <samox0918@gmail.com>
2020-12-11 10:02:31 +0800

    cp4 finish

--- checkpoint4/demo.cpp
+++ checkpoint4/demo.cpp
@@ -29,7 +29,7 @@ double goal_ir_ratio = 0.19;
 #define END 5
 #define CAPTURE_LED 7
 #define FIND_IR 8
-
+#define SPINFIX_REV 9
 
 #define ON_LIGHT 0
 #define OFF_LIGHT 1
@@ -59,6 +59,12 @@ void MoveBackRight(ros::Publisher& Lmotor_publisher, ros::Publisher& Rmotor_publ
 void MoveStop(ros::Publisher& Lmotor_publisher, ros::Publisher& Rmotor_publisher){
 	msgL.data = 0;
 	msgR.data = 0;
+	Lmotor_publisher.publish(msgL);
+	Rmotor_publisher.publish(msgR);
+}
+void MoveSpinSlow(ros::Publisher& Lmotor_publisher, ros::Publisher& Rmotor_publisher){
+	msgL.data = -15;
+	msgR.data = 15;
 	Lmotor_publisher.publish(msgL);
 	Rmotor_publisher.publish(msgR);
 }
@@ -106,24 +112,27 @@ void MoveBackward(ros::Publisher& Lmotor_publisher, ros::Publisher& Rmotor_publi
 float calculate_IR_ratio(ros::Rate& loop_rate){
   double lastms,count_0=0,count_1=0,beacon=0;
 	//ros::Rate loop_rate(25);
-  while(ros::ok()){
-  lastms=ros::Time::now().toSec();
-   while (ros::Time::now().toSec()-lastms<0.04){
-     int IR_data = digitalRead(IRReceiverPin);
-     if(IR_data == 0)
-          count_0++;
-     else if(IR_data == 1)
-          count_1++;
+  // while(ros::ok()){
+	lastms=ros::Time::now().toSec();
+	//cout << lastms << endl;
+    while (ros::Time::now().toSec()-lastms<0.05){
+		//cout << ros::Time::now().toSec() << endl;
+		int IR_data = digitalRead(IRReceiverPin);
+		if(IR_data == 0)
+			count_0++;
+		else if(IR_data == 1)
+			count_1++;
     }
+	
     beacon = (count_0)/(count_1+count_0);
+	cout << "(" << count_0<< "/"<<count_1<<")"<< beacon <<endl;
     //ROS_INFO("beacon read : %1f",beacon);
-    count_0=0;
-    count_1=0;  
-    loop_rate.sleep();
-  }
+    // count_0=0;
+    // count_1=0;  
+    // loop_rate.sleep();
+  // }
   return beacon;
-}	
-
+}
 
 int _light;
 void number_callback(const std_msgs::Int32& msg){
@@ -147,21 +156,23 @@ int main(int argc, char **argv)
 	int number_count = 0,velL=0,velR=0;
     //setenv("WIRINGPI_GPIOMEM","1",1);
 	wiringPiSetup();
-	
+	cout << "main 1" << endl;
 	pinMode(LightPin,INPUT);
 	pinMode(TouchPinL,INPUT);
 	pinMode(TouchPinR,INPUT);
 	pinMode(TouchPinB,INPUT);
 	pinMode(IRReceiverPin,INPUT);
+	
+	int SPINFIX_REV_threshold = 10;
 
 	
-	string STATUS_STRING[] = {"COLLIDE", "BACK", "FIND_LED", "SPIN", "FORWARD","END", "FIND_IR","CAPTURE_LED"};
+	string STATUS_STRING[] = {"COLLIDE", "BACK", "FIND_LED", "SPIN", "FORWARD","END", "SPINFIX","CAPTURE_LED", "FIND_IR", "SPINFIX_REV"};
 	//Current state of each pin
     int _TouchPinL=0, _TouchPinR=0, _TouchPinB=0,min_light=900;
     double _IRReceiverPin=0,L_IRReceiverPin=0;
 	//Last state of each pin
     int L_light=0, L_TouchPinL=0, L_TouchPinR=0, L_TouchPinB=0, L_status=0 ;
-	
+	cout << "main 2" << endl;
 	int status;
 	if(argc == 2) //if argc == 2 DEBUG mode is ON.
 		status = END;
@@ -173,16 +184,28 @@ int main(int argc, char **argv)
 		cout << "set _light_val to " << _light_val;
 		goal_ir_ratio = atof(argv[2]);
 	}
- 
+	if(argc == 4){
+		_light_val = atoi(argv[1]);
+		cout << "set _light_val to " << _light_val;
+		goal_ir_ratio = atof(argv[2]);		
+		SPINFIX_REV_threshold = atoi(argv[3]);
+	}
+	cout << "main 3" << endl;
 	int last_state_number_count = 0;
 	int SPIN_random, randomed=0;
 	int backstatus = 0;
-	
+	cout << "main 4" << endl;
 	MoveStop(Lmotor_publisher, Rmotor_publisher);
+	cout << "main 4.1" << endl;
 	cout << "init status " << STATUS_STRING[status].c_str() << endl;
+	cout << "main 4.2" << endl;
 	ros::Subscriber number_subscribe = node_obj.subscribe("/ard2rpi", 1, number_callback);
+	cout << "main 4.3" << endl;
 	string current_target("LED");
-  
+	cout << "main 4.4" << endl;
+	cout << "main 5" << endl;
+	int first_round = 1;
+	
 	while (ros::ok())
 	{
 		L_light = _light;
@@ -305,22 +328,42 @@ int main(int argc, char **argv)
 					cout << "SPIN to find min" << endl;
 				}
 				else{
+					ROS_INFO("LightPin: %d",_light);
 					MoveSpinFast(Lmotor_publisher, Rmotor_publisher);
 					if(_light < min_light)
 						min_light = _light;
 					status = FIND_LED;
+					// if(first_round && _light < _light_val){
+						// status = FIND_LED;
+						// IsLightOn = true;
+						// first_round = 0;
+					// }
 				}
 				break;
-			case SPINFIX:
-				if(number_count - last_state_number_count > 5){
-					cout << "REV finish" << endl;
+			case SPINFIX_REV:
+				if(number_count - last_state_number_count > SPINFIX_REV_threshold){
+					cout << "SPINFIX_REV finish" << endl;
 					MoveStop(Lmotor_publisher, Rmotor_publisher);
 					last_state_number_count = number_count;
 					status = FORWARD;
 					cout << "Turn to FORWARD" << endl;
 				}
 				else{
+					cout << "SPINFIX_REV_threshold = " << SPINFIX_REV_threshold << endl;
 					MoveSpin(Lmotor_publisher, Rmotor_publisher);
+				}
+				break;
+
+			case SPINFIX:
+				if(number_count - last_state_number_count > 5){
+					cout << "SPINFIX finish" << endl;
+					MoveStop(Lmotor_publisher, Rmotor_publisher);
+					last_state_number_count = number_count;
+					status = FORWARD;
+					cout << "Turn to FORWARD" << endl;
+				}
+				else{
+					MoveSpinRev(Lmotor_publisher, Rmotor_publisher);
 				}
 				break;
 			case SPIN:
@@ -370,12 +413,12 @@ int main(int argc, char **argv)
 			case FIND_IR:
 				cout << "FIND_IR" << endl;
 				_IRReceiverPin=calculate_IR_ratio(loop_rate);
-				if( (_IRReceiverPin>goal_ir_ratio-0.03) && (_IRReceiverPin<goal_ir_ratio+0.03)){
+				if( (_IRReceiverPin>goal_ir_ratio-0.02) && (_IRReceiverPin<goal_ir_ratio+0.02)){
 					cout << "FIND IR!!" << endl;
 					MoveStop(Lmotor_publisher, Rmotor_publisher);
-					last_state_number_count = number_count;
-					status = SPINFIX;
-					cout << "Turn to SPINFIX" << endl;
+					last_state_number_count = number_count ;
+					status = FORWARD;
+					cout << "Turn to FORWARD" << endl;
 				}
 				else if(_TouchPinL || _TouchPinR){
 					cout << "Collide while spin" << endl;
@@ -384,7 +427,7 @@ int main(int argc, char **argv)
 					status = COLLIDE;
 					cout << "Turn to COLLIDE" << endl;
 				}
-				else if(number_count - last_state_number_count > 50){
+				else if(number_count - last_state_number_count > 130){
 					cout << "CANNOT FIND ir" << endl;
 					MoveStop(Lmotor_publisher, Rmotor_publisher);
 					last_state_number_count = number_count;
@@ -392,7 +435,7 @@ int main(int argc, char **argv)
 					cout << "Turn to SPIN" << endl;
 				}
 				else{
-					MoveSpin(Lmotor_publisher, Rmotor_publisher);
+					MoveSpinSlow(Lmotor_publisher, Rmotor_publisher);
 					status = FIND_IR;
 				} 
 				break;
@@ -410,5 +453,4 @@ int main(int argc, char **argv)
 	}
 	return 0;
 }
-
 
