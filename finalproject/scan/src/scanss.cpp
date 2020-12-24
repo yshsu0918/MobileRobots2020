c0b8ed74555560f2826da2b4a8e162995fcfd299
sam hsu <a@b.c>
2020-12-25 02:56:33 +0800

    final ver

--- finalproject/scan/src/scanss.cpp
+++ finalproject/scan/src/scanss.cpp
@@ -1,45 +1,46 @@
+
 #include <ros/ros.h>  
 #include <iostream>
-#include <sensor_msgs/LaserScan.h>
 #include <cmath>
 #include <cstdlib>
+#include <sensor_msgs/LaserScan.h>
+
+#include "std_msgs/MultiArrayLayout.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "std_msgs/Int32MultiArray.h"
 #include "std_msgs/Int32.h"
+
+#define MAX_DIRECTIONS 72
+#define RAD2DEG(x) ((x)*180./M_PI)
+
 using namespace std;
 
-int forward = 0, right_forward = 0, left_forward = 0, right = 0, left = 0, status = 0;
 std_msgs::Int32 msgL;
 std_msgs::Int32 msgR;
 ros::Publisher Lmotor_publisher;
 ros::Publisher Rmotor_publisher;
 
-//Get laser data callback function
-int min_lidar(int number, int range, const sensor_msgs::LaserScan& laser) {
-    int min_count = 0;
-    for (int i = number - range / 2; i < number + range / 2; i++) {
-        if (laser.ranges[i]<0.3) {
-            min_count++;
-        }
-    }
-    return min_count;
-}
-
-
-#define MAX_DIRECTIONS 12
-#define RAD2DEG(x) ((x)*180./M_PI)
+#define MID_BLOCK 1
+#define MID_GO 2
+#define FINISH 3
 int result[MAX_DIRECTIONS];
-
-bool InInterval(float mid, float range, float ask){
-	return abs(mid - ask) < range || abs(mid - ask + 360) < range || abs(mid - ask - 360) < range;
-}
 float min_[MAX_DIRECTIONS];
+int camera_ret[5];
+
+void get_camera_callback(const std_msgs::Int32MultiArray::ConstPtr& array){
+	int i = 0;
+	// print all the remaining numbers
+	for(std::vector<int>::const_iterator it = array->data.begin(); it != array->data.end(); ++it){
+		camera_ret[i] = *it;
+		i++;
+	}
+	//cout << "Camera ret " << camera_ret[0] << "/" << camera_ret[1] << endl;
+}
+
 void get_laser_callback(const sensor_msgs::LaserScan::ConstPtr& scan) {
 
     int count = scan->scan_time / scan->time_increment;
-    // ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
-    // ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
-	
-	// int left = 0,left_forward=0, right_forward=0, right =0 , forward = 0;
-	
+
 	float a[MAX_DIRECTIONS];
 	int b[MAX_DIRECTIONS];
 	
@@ -65,42 +66,6 @@ void get_laser_callback(const sensor_msgs::LaserScan::ConstPtr& scan) {
 			min_[t] = empty_pnt;
 		}
     }
-
-	float buf[MAX_DIRECTIONS];
-	//system("clear");
-	for(int i =0;i< MAX_DIRECTIONS;i++){
-		// cout << "a " << i << ":"<< a[i] << endl; 
-		// cout << "b " << i << ":"<< b[i] << endl; 
-		//cout << "a/b " << i << ":"<< a[i]/float(b[i]) << endl; 
-		// cout << i*30 << "-" << i*30+30 << ":"<< min_[i] << endl;
-	}
-	
-	// for(int i= 0;i<MAX_DIRECTIONS;i++){
-		// cout << "dir " << i << ":"<<buf[i] << endl; 
-		// result[i] = buf[i] > 3 ? 1: 0;
-	// }
-	
-    // if (left > 3)
-        // reuslt = 0
-    // if (left_forward > 3)
-        // status += 2;
-    // if (forward > 3)
-        // status += 4;
-    // if (right_forward > 3)
-        // status += 8;
-    // if (right > 3)
-        // status += 16;
-	
-	
-
-    // cout << "ROS Lidar Data" << endl;
-    // cout << "Left:" << left << endl;
-    // cout << "left_forward:" << left_forward << endl;
-    // cout << "Straight ahead:" << forward << endl;
-    // cout << "right_forward:" << right_forward << endl;
-    // cout << "right:" << right << endl;
-    // cout << "status: " << status << endl;
-    // cout << "-----------------" << endl;
 }
 
 
@@ -111,62 +76,104 @@ int main(int argc, char** argv)
     ros::NodeHandle n;
     Lmotor_publisher = n.advertise<std_msgs::Int32>("/receive_msgL", 1);
     Rmotor_publisher = n.advertise<std_msgs::Int32>("/receive_msgR", 1);
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(10);
     // Subscribe to the ros topic in gazebo and set the callback function
     ros::Subscriber laser_sub = n.subscribe("/scan", 10 , get_laser_callback);
+	ros::Subscriber camera_sub = n.subscribe("/point_x_scale", 10 , get_camera_callback);
 	
 	float avoid_distance;
-	if(argc > 1)
-		avoid_distance = atof(argv[1]);
-	else
-		avoid_distance = 0.5;
-	
+	avoid_distance = atof(argv[1]);
+	int move_unit = atoi(argv[2]);
+	int obj_size = atoi(argv[3]);
+	int ignore_block = 0;
+
+	int status = MID_GO;
     while (ros::ok())
     {
 		cout << "--------------------------------- " << endl;
 		int final_direction = 0, min_distance = 10000, min_index = -1;
 		int current_distance;
-		
 		int forward_section = MAX_DIRECTIONS/2;
-		int start_section = 5;
-		int middle_section = 8;
-		int end_section = 12;
-		for(int i = start_section; i < end_section ; i++){
-			if(min_[i] < avoid_distance ){
-				cout << "Direction " << i << " is blocked"<< endl;
-				continue;
-			}
-			current_distance = abs( middle_section - i);
-			if(min_distance > current_distance){
-				min_distance = current_distance;
-				min_index = i;
-			}
-		}
-		cout << "The nearest non-blocked direction to central is " << min_index << ".\n";
-		if(min_index == -1){
-			msgL.data = -50;
-			msgR.data = -50;
-		}
-		else{
-			int move_unit = 10;
-			int Q = min_index - start_section;
-			msgR.data = (forward_section - Q + 1) * move_unit;
-			msgL.data = (Q + 1) * move_unit;
-
-		}
-		cout << "Go L/R: " << msgL.data << "/" << msgR.data <<endl;
+		int start_section = 36;
+		int middle_section = 54;
+		int mid_interval = 5;
+		int end_section = 72;
+		bool middle_block = false;
+		bool midblock_flag = false;
 		
-		if(argc > 1 && avoid_distance < 0){
+		for(int i = middle_section - mid_interval; i <  middle_section + mid_interval + 1 ; i++){
+			cout << i* (360/MAX_DIRECTIONS) << ":" <<min_[i]<< ":"<< avoid_distance <<endl;
+			if(min_[i] < avoid_distance)
+				midblock_flag = true;
+		}
+
+		int object_center_in_width = camera_ret[1] < 1000 ? -1: camera_ret[0];
+		int object_width = camera_ret[1];
+		int camera_width = 640;
+		int divide = 10;
+
+		if(camera_ret[1] > obj_size){
+			ignore_block = 1;
+		}
+		if(camera_ret[1] > obj_size*3){
+			status = FINISH;
+		}
+
+
+		switch(status){
+			case FINISH:
+				msgL.data = 0;
+				msgR.data = 0;
+				status = FINISH;
+				break;
+			case MID_BLOCK:
+				cout << "MID_BLOCK" <<endl;
+				if(midblock_flag){
+					msgL.data = -15;
+					msgR.data = 15;
+					status = MID_BLOCK;					
+				}
+				else{
+					status = MID_GO;
+				}
+				break;
+			case MID_GO:
+				cout << "MID_GO" <<endl;
+				if(ignore_block == 0 && midblock_flag){
+					status =  MID_BLOCK ;
+				}
+				else{
+					//Search target in the carmera;
+
+					int part = int ( object_center_in_width / (camera_width / divide));
+					cout << "Camera ret " << camera_ret[0] << "/" << camera_ret[1] << "/" << obj_size << endl;
+					if(object_center_in_width == -1){
+						msgL.data = 50;
+						msgR.data = 50;
+						cout << "Search target fail , Go straight: " << msgL.data << "/" << msgR.data <<endl;
+					}
+					else{
+						msgR.data = (divide - part + 1) * move_unit;
+						msgL.data = (part + 1) * move_unit;	
+						cout << "Part " << part <<endl;
+						cout << "By Search target, Go L/R: " << msgL.data << "/" << msgR.data <<endl;
+					}
+					status = MID_GO;
+				}
+				break;
+		}
+
+		if(argc > 4){
+			//debug mode, don't publish th msg to motor.
 		}
 		else{
 			Rmotor_publisher.publish(msgR);
 			Lmotor_publisher.publish(msgL);
 		}
-        
-        
+
         ros::spinOnce();
         loop_rate.sleep();
-    // callback rotation
+		// callback rotation
     }
     return 0;
 }
